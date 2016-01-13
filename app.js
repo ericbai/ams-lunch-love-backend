a4cdb6df890a121bfcd9907942ee03c4ec926df8
Eric <heyericbai@gmail.com>
2016-01-13 01:00:40 -0800

    added PORT as environment variable in app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ db.listAllLabels((err, result) => {
 	}
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
 	const port = server.address().port;
 	console.log(`Running on port ${port}`);
 });
